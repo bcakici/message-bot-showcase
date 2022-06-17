6686d3b1daa444c4bf4f5d4779adb51b5081d467
Berkay Cakici <cakici.berkay@gmail.com>
2022-06-17 18:26:29 +0300

    Make dashboard a different component

---
+++ components/Dashboard.tsx
@@ -0,0 +1,42 @@
+export default function Dashboard() {
+	return (
+		<div className="w-screen lg:flex-grow lg:w-2/3 p-10">
+			<h2 className="mb-10 font-thin text-xl">Are customers satisfied?</h2>
+			<div className="flex flex-col gap-5">
+				<div className="flex justify-between">
+					<div className="flex flex-1 text-sm font-semibold text-gray-400">
+						Customer
+					</div>
+					<div className="flex flex-1 text-sm font-semibold text-gray-400">
+						Transaction completed
+					</div>
+					<div className="flex flex-1 text-sm font-semibold text-gray-400">
+						Happy
+					</div>
+					<div className="flex flex-1 text-sm font-semibold text-gray-400">
+						Out of 5
+					</div>
+					<div className="flex flex-1 text-sm font-semibold text-gray-400">
+						Review Note
+					</div>
+				</div>
+				<div className="flex justify-between">
+					<div className="flex flex-1">Berkay</div>
+					<div className="flex flex-1">At 05.05.2022</div>
+					<div className="flex flex-1">😁</div>
+					<div className="flex flex-1">5/5</div>
+					<div className="flex flex-1">It's peeerfect.</div>
+				</div>
+				<div className="flex justify-between">
+					<div className="flex flex-1">Joe</div>
+					<div className="flex flex-1">
+						<a>Mark as completed</a>
+					</div>
+					<div className="flex flex-1">:(</div>
+					<div className="flex flex-1">2/5</div>
+					<div className="flex flex-1">Could be better.</div>
+				</div>
+			</div>
+		</div>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect } from "react";
+import Dashboard from "../components/Dashboard";
 import MessageBox from "../components/MessageBox";
 import useScenario from "../hooks/useScenario";
 
@@ -21,46 +22,7 @@ const Home: NextPage = () => {
 
 			<div className="flex w-full min-h-screen bg-gray-800 text-white p-10 overflow-x-clip">
 				<div className="flex flex-col justify-center lg:flex-row lg:w-full lg:max-w-screen-2xl mx-auto">
-					<div className="w-screen lg:flex-grow lg:w-2/3 p-10">
-						<h2 className="mb-10 font-thin text-xl">
-							Are customers satisfied?
-						</h2>
-						<div className="flex flex-col gap-5">
-							<div className="flex justify-between">
-								<div className="flex flex-1 text-sm font-semibold text-gray-400">
-									Customer
-								</div>
-								<div className="flex flex-1 text-sm font-semibold text-gray-400">
-									Transaction completed
-								</div>
-								<div className="flex flex-1 text-sm font-semibold text-gray-400">
-									Happy
-								</div>
-								<div className="flex flex-1 text-sm font-semibold text-gray-400">
-									Out of 5
-								</div>
-								<div className="flex flex-1 text-sm font-semibold text-gray-400">
-									Review Note
-								</div>
-							</div>
-							<div className="flex justify-between">
-								<div className="flex flex-1">Berkay</div>
-								<div className="flex flex-1">At 05.05.2022</div>
-								<div className="flex flex-1">😁</div>
-								<div className="flex flex-1">5/5</div>
-								<div className="flex flex-1">It's peeerfect.</div>
-							</div>
-							<div className="flex justify-between">
-								<div className="flex flex-1">Joe</div>
-								<div className="flex flex-1">
-									<a>Mark as completed</a>
-								</div>
-								<div className="flex flex-1">:(</div>
-								<div className="flex flex-1">2/5</div>
-								<div className="flex flex-1">Could be better.</div>
-							</div>
-						</div>
-					</div>
+					<Dashboard />
 					<div className="flex flex-col lg:w-1/3 h-5/6">
 						<div className="flex my-10">
 							<a
